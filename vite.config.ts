4084ead3530eef9291f5f2406dea5be0b4f4ee27
kartheeswaranAR <kartheeswaran.dev@outloook.com>
2025-09-22 19:28:14 +0530

    RK-E018-Added the config

--- vite.config.ts
+++ vite.config.ts
@@ -5,22 +5,21 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/aiadmk/",
   server: {
     host: "::",
     port: 8080,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
